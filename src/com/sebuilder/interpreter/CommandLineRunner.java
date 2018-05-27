b33c9d0dcbe651097063ea25992c3185eb6311b3
y.onodera <yutaonodera0304@gmail.com>
2018-05-27 15:07:34 +0900

    コマンドラインからstepを入力しての操作を追加

---
+++ src/com/sebuilder/interpreter/CommandLineRunner.java
@@ -0,0 +1,69 @@
+package com.sebuilder.interpreter;
+
+import com.sebuilder.interpreter.factory.ScriptFactory;
+import com.sebuilder.interpreter.factory.StepTypeFactory;
+import com.sebuilder.interpreter.factory.TestRunFactory;
+import com.sebuilder.interpreter.webdriverfactory.Firefox;
+import com.sebuilder.interpreter.webdriverfactory.WebDriverFactory;
+import org.apache.commons.logging.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+public abstract class CommandLineRunner {
+    protected static WebDriverFactory DEFAULT_DRIVER_FACTORY = new Firefox();
+    protected ScriptFactory sf = new ScriptFactory();
+    protected StepTypeFactory stf = new StepTypeFactory();
+    protected TestRunFactory trf = new TestRunFactory();
+    protected HashMap<String, String> driverConfig = new HashMap<>();
+    protected WebDriverFactory wdf = DEFAULT_DRIVER_FACTORY;
+    protected TestRun lastRun = null;
+    protected SeInterpreterTestListener seInterpreterTestListener = new SeInterpreterTestListener();
+    protected Log log;
+
+    protected CommandLineRunner(String[] args, Log log) {
+        this.log = log;
+        if (args.length == 0) {
+            log.info("Usage: [--driver=<drivername] [--driver.<configkey>=<configvalue>...] [--implicitlyWait=<ms>] [--pageLoadTimeout=<ms>] [--stepTypePackage=<package name>] <script path>...");
+            System.exit(0);
+        }
+        sf.setStepTypeFactory(stf);
+        sf.setTestRunFactory(trf);
+        for (String s : args) {
+            if (s.startsWith("--")) {
+                String[] kv = s.split("=", 2);
+                if (kv.length < 2) {
+                    log.fatal("Driver configuration option \"" + s + "\" is not of the form \"--driver=<name>\" or \"--driver.<key>=<value\".");
+                    System.exit(1);
+                }
+                if (s.startsWith("--implicitlyWait")) {
+                    trf.setImplicitlyWaitDriverTimeout(Long.valueOf(kv[1]));
+                } else if (s.startsWith("--pageLoadTimeout")) {
+                    trf.setPageLoadDriverTimeout(Long.valueOf(kv[1]));
+                } else if (s.startsWith("--stepTypePackage")) {
+                    stf.setPrimaryPackage(kv[1]);
+                } else if (s.startsWith("--driver.")) {
+                    driverConfig.put(kv[0].substring("--driver.".length()), kv[1]);
+                } else if (s.startsWith("--driver")) {
+                    try {
+                        wdf = (WebDriverFactory) Class.forName("com.sebuilder.interpreter.webdriverfactory." + kv[1]).getDeclaredConstructor().newInstance();
+                    } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
+                        log.fatal("Unknown WebDriverFactory: " + "com.sebuilder.interpreter.webdriverfactory." + kv[1], e);
+                    } catch (InstantiationException | IllegalAccessException e) {
+                        log.fatal("Could not instantiate WebDriverFactory " + "com.sebuilder.interpreter.webdriverfactory." + kv[1], e);
+                    }
+                } else {
+                    configureOption(s, kv);
+                }
+            } else {
+                configureOption(s);
+            }
+        }
+    }
+
+    protected void configureOption(String s, String[] kv) {
+    }
+
+    protected void configureOption(String s) {
+    }
+}
---
+++ src/com/sebuilder/interpreter/SeInterpreterREPL.java
@@ -0,0 +1,129 @@
+package com.sebuilder.interpreter;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class SeInterpreterREPL extends CommandLineRunner {
+    private RemoteWebDriver driver;
+
+    public SeInterpreterREPL(String[] args, Log log) {
+        super(args, log);
+    }
+
+    public static void main(String[] args) {
+        Log log = LogFactory.getFactory().getInstance(SeInterpreterREPL.class);
+        SeInterpreterREPL interpreter = new SeInterpreterREPL(args, log);
+        try {
+            interpreter.run();
+        } catch (Exception e) {
+            log.fatal("Run error.", e);
+            System.exit(1);
+        }
+    }
+
+    private void run() {
+        setupREPL();
+        runningREPL();
+        tearDownREPL();
+    }
+
+    private void setupREPL() {
+        this.seInterpreterTestListener.cleanResult();
+        this.seInterpreterTestListener.openTestSuite("REPL", new Hashtable<>());
+    }
+
+    private void runningREPL() {
+        final Scanner scanner = new Scanner(System.in);
+        StringBuilder input = null;
+        boolean commandInput = false;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine().trim();
+            if (line.equals("exit")) {
+                break;
+            } else if (!commandInput && line.startsWith("@")) {
+                List<Script> scripts = loadScript(line.substring(1));
+                scripts.forEach(script -> execute(script));
+            } else if (!commandInput && line.startsWith("{")) {
+                input = startScript();
+                input.append(line);
+                commandInput = true;
+            } else if (commandInput && line.equals("/")) {
+                closeScript(input);
+                commandInput = false;
+                Script script = toScript(input.toString());
+                input = null;
+                if (script != null) {
+                    execute(script);
+                }
+            } else if (commandInput) {
+                input.append(line);
+            }
+        }
+    }
+
+    private void tearDownREPL() {
+        if (this.driver != null) {
+            this.driver.quit();
+        }
+        this.seInterpreterTestListener.closeTestSuite();
+        this.seInterpreterTestListener.aggregateResult();
+    }
+
+    private void execute(Script script) {
+        script.stateTakeOver();
+        lastRun = script.createTestRun(this.log, wdf, driverConfig, new HashMap<>(), lastRun, seInterpreterTestListener);
+        if (this.driver == null) {
+            this.driver = lastRun.driver();
+        }
+        while (lastRun.hasNext()) {
+            try {
+                if (lastRun.next()) {
+                    this.log.info(lastRun.currentStep().toPrettyString() + " succeeded");
+                } else {
+                    this.log.info(lastRun.currentStep().toPrettyString() + " failed");
+                }
+            } catch (AssertionError e) {
+                this.log.error("error " + lastRun.currentStep().toPrettyString(), e);
+            }
+        }
+    }
+
+    private List<Script> loadScript(String file) {
+        List<Script> result = Lists.newArrayList();
+        try {
+            result = sf.parse(new File(file));
+        } catch (IOException | JSONException e) {
+            this.log.error(e);
+        }
+        return result;
+    }
+
+    private Script toScript(String cmdInput) {
+        Script result = null;
+        try {
+            result = sf.parse(cmdInput);
+        } catch (IOException | JSONException e) {
+            this.log.error(e);
+        }
+        if (result == null) {
+            this.log.error("invalid input:" + cmdInput);
+        }
+        return result;
+    }
+
+    private StringBuilder startScript() {
+        return new StringBuilder().append("{\"steps\":[");
+    }
+
+    private void closeScript(StringBuilder script) {
+        script.append("]}");
+    }
+
+}
