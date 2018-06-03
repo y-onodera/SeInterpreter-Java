b2e9911391d960a536152406f60cd74ec2765452
y.onodera <yutaonodera0304@gmail.com>
2018-06-03 13:45:00 +0900

    要素の調査のためのstepを追加

---
+++ src/com/sebuilder/interpreter/steptype/HighLightElement.java
@@ -0,0 +1,104 @@
+package com.sebuilder.interpreter.steptype;
+
+import com.google.common.collect.Maps;
+import com.sebuilder.interpreter.Locator;
+import com.sebuilder.interpreter.StepType;
+import com.sebuilder.interpreter.TestRun;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.WebElement;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class HighLightElement implements StepType {
+    /**
+     * Perform the action this step consists of.
+     *
+     * @param ctx Current test run.
+     * @return Whether the step succeeded. This should be true except for failed verify steps, which
+     * should return false. Other failures should throw a RuntimeException.
+     */
+    @Override
+    public boolean run(TestRun ctx) {
+        Cache.reflesh(ctx);
+        for (WebElement element : ctx.locator().findElements(ctx)) {
+            Cache.originalStyle(ctx, element);
+            toggleHighlight(ctx, element, "2px solid red");
+        }
+        return true;
+    }
+
+    static void toggleHighlight(TestRun ctx, WebElement element, String aStyle) {
+        toggleBorder(ctx, element, aStyle);
+        if (isChangeOutline(element)) {
+            toggleOutline(ctx, element, aStyle);
+        }
+    }
+
+    private static boolean isChangeOutline(WebElement element) {
+        String type = element.getAttribute("type");
+        return Objects.equals(type, "checkbox") || Objects.equals(type, "radio");
+    }
+
+    private static void toggleOutline(TestRun ctx, WebElement element, String aStyle) {
+        ctx.driver().executeScript("arguments[0].style.outline = '" + aStyle + "'; return [];", element);
+    }
+
+    private static void toggleBorder(TestRun ctx, WebElement element, String aStyle) {
+        ctx.driver().executeScript("arguments[0].style.border = '" + aStyle + "'; return [];", element);
+    }
+
+    public static class Cache {
+        private static String currentUrl;
+        static Map<Locator, BackupStyle> cache = Maps.newHashMap();
+
+        public static void reflesh(TestRun aCtx) {
+            String newUrl = aCtx.driver().getCurrentUrl();
+            if (Objects.equals(currentUrl, newUrl)) {
+                cache.entrySet()
+                        .stream()
+                        .forEach(entry -> {
+                            entry.getKey().
+                                    findElements(aCtx)
+                                    .stream()
+                                    .forEach(element -> {
+                                        BackupStyle style = entry.getValue();
+                                        toggleBorder(aCtx, element, style.border);
+                                        if (isChangeOutline(element)) {
+                                            toggleOutline(aCtx, element, style.outline);
+                                        }
+                                    });
+                        });
+            }
+            currentUrl = newUrl;
+            cache.clear();
+        }
+
+        public static void originalStyle(TestRun ctx, WebElement element) {
+            String originalBorder = (String) ctx.driver().executeScript("return arguments[0].style.border;", element);
+            String originalOutline = null;
+            if (isChangeOutline(element)) {
+                originalOutline = (String) ctx.driver().executeScript("return arguments[0].style.outline;", element);
+            }
+            try {
+                JSONObject json = Locator.toJson(ctx.driver(), element);
+                Locator locator = new Locator(json.getString("type"), json.getString("value"));
+                cache.put(locator, new BackupStyle(originalBorder, originalOutline));
+            } catch (JSONException e) {
+                ctx.log().error(e);
+            }
+        }
+
+    }
+
+    private static class BackupStyle {
+        private String border;
+        private String outline;
+
+        public BackupStyle(String border, String outline) {
+            this.border = border;
+            this.outline = outline;
+        }
+    }
+}
