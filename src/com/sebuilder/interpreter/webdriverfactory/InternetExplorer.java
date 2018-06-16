43c550bfb559baa4c8912ea42d0fe6c9a02bde21
y.onodera <yutaonodera0304@gmail.com>
2018-06-16 21:53:10 +0900

    InternetExplorer用のdriverを追加

---
+++ src/com/sebuilder/interpreter/webdriverfactory/InternetExplorer.java
@@ -0,0 +1,21 @@
+package com.sebuilder.interpreter.webdriverfactory;
+
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.HashMap;
+
+public class InternetExplorer implements WebDriverFactory{
+    /**
+     * @param config A key/value mapping of configuration options specific to this factory.
+     * @return A RemoteWebDriver of the type produced by this factory.
+     */
+    @Override
+    public RemoteWebDriver make(HashMap<String, String> config) throws Exception {
+        HashMap<String, String> caps = new HashMap<String, String>(config);
+        DesiredCapabilities capabilities = new DesiredCapabilities(caps);
+        return new InternetExplorerDriver(new InternetExplorerOptions(capabilities));
+    }
+}
