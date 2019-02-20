7d0087562afc4c030dad7730a88f780bc8b7841e
y.onodera <yutaonodera0304@gmail.com>
2019-02-21 04:55:43 +0900

    困ったとき用のdocumentreadyを用意

---
+++ src/main/java/com/sebuilder/interpreter/steptype/DocumentReady.java
@@ -0,0 +1,26 @@
+package com.sebuilder.interpreter.steptype;
+
+import com.sebuilder.interpreter.Getter;
+import com.sebuilder.interpreter.LocatorHolder;
+import com.sebuilder.interpreter.TestRun;
+
+public class DocumentReady implements Getter, LocatorHolder {
+
+    /**
+     * @param ctx Current test run.
+     * @return The value this getter gets, eg the page title.
+     */
+    @Override
+    public String get(TestRun ctx) {
+        return String.valueOf(ctx.driver().executeScript("return document.readyState").equals("complete"));
+    }
+
+    /**
+     * @return The name of the parameter to compare the result of the get to, or null if the get
+     * returns a boolean "true"/"false".
+     */
+    @Override
+    public String cmpParamName() {
+        return null;
+    }
+}
