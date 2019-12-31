065cd5f4e459be0de03e3471f0ec81052327ab02
y-onodera <yutaonodera0304@gmail.com>
2019-12-31 12:51:31 +0900

    rename

--- src/test/java/com/sebuilder/interpreter/InputDataTest.java
+++ src/test/java/com/sebuilder/interpreter/InputDataTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestDataInput {
+public class InputDataTest {
 
     private InputData target = new InputData()
             .add("aTrue", "true")
