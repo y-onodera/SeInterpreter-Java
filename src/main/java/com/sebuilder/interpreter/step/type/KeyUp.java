ecc1b3ac5e936b04765ccddca6ad9b6b3350e5f8
y-onodera <yutaonodera0304@gmail.com>
2020-11-23 17:14:13 +0900

    キーボード操作を追加

---
+++ src/main/java/com/sebuilder/interpreter/step/type/KeyUp.java
@@ -0,0 +1,23 @@
+package com.sebuilder.interpreter.step.type;
+
+import com.sebuilder.interpreter.StepBuilder;
+import com.sebuilder.interpreter.TestRun;
+import com.sebuilder.interpreter.step.AbstractStepType;
+
+public class KeyUp extends AbstractStepType {
+
+    @Override
+    public boolean run(TestRun ctx) {
+        ctx.driver().getKeyboard().releaseKey(ctx.text());
+        return true;
+    }
+
+    @Override
+    public StepBuilder addDefaultParam(StepBuilder o) {
+        if (!o.containsStringParam("text")) {
+            o.put("text", "");
+        }
+        return o;
+    }
+
+}
