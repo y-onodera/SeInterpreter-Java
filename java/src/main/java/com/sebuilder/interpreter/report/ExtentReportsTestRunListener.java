3145528c75a9f3caee249677dd37525500342961
y-onodera <yutaonodera0304@gmail.com>
2022-05-01 06:28:47 +0900

    extentReportの日本語が化けるのを修正

--- java/src/main/java/com/sebuilder/interpreter/report/ExtentReportsTestRunListener.java
+++ java/src/main/java/com/sebuilder/interpreter/report/ExtentReportsTestRunListener.java
@@ -3,13 +3,22 @@ package com.sebuilder.interpreter.report;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.io.BufferedWriterWriter;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.ViewName;
+import com.aventstack.extentreports.templating.FreemarkerTemplate;
 import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class ExtentReportsTestRunListener extends TestRunListenerImpl {
@@ -49,6 +58,31 @@ public class ExtentReportsTestRunListener extends TestRunListenerImpl {
             @Override
             protected Configuration createFreemarkerConfig(String templatePath, String encoding) {
                 return super.createFreemarkerConfig("custom/" + templatePath, encoding);
+            }
+
+            @Override
+            protected void processTemplate(Template template, File outputFile) throws TemplateException, IOException {
+                String encoding = this.getFreemarkerConfig().getDefaultEncoding();
+                FreemarkerTemplate freemarkerTemplate = new FreemarkerTemplate(this.getFreemarkerConfig()) {
+                    @Override
+                    public void writeTemplate(Template template, Map<String, Object> templateMap, File outputFile) throws TemplateException, IOException {
+                        String source = this.getSource(template, templateMap);
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, Charset.forName(encoding)));
+                        try {
+                            writer.write(source);
+                        } catch (Throwable var7) {
+                            try {
+                                writer.close();
+                            } catch (Throwable var6) {
+                                var7.addSuppressed(var6);
+                            }
+
+                            throw var7;
+                        }
+                        writer.close();
+                    }
+                };
+                freemarkerTemplate.writeTemplate(template, this.getTemplateModel(), outputFile);
             }
         }.viewConfigurer()
                 .viewOrder()
